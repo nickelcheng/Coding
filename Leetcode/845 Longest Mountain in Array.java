4e1028bdb0fb9185ae1ee3b5dbfb98cca1b8de90
Nickel Cheng <nickelcheng@users.noreply.github.com>
2020-11-17 18:12:59 +0000

    A better solution :D

--- Leetcode/845 Longest Mountain in Array.java
+++ Leetcode/845 Longest Mountain in Array.java
@@ -1,59 +1,25 @@
 class Solution {
-    class Mountain {
-        boolean up;
-        boolean down;
-        int size;
-        int curr;
-        public Mountain() {
-            up = false;
-            down = false;
-            size = 0;
-            curr = -1;
-        }
-        int tryNextPoint(int pt) {
-            size++;
-            if (curr == -1) {
-                curr = pt;
-                return 0;
-            }
-            if (!up && pt > curr) {
-                curr = pt;
-                up = true;
-                return 0;
-            }
-            if (up && !down) {
-                if (pt > curr) {
-                    curr = pt;
-                    return 0;
-                }
-                if (pt < curr) {
-                    down = true;
-                    curr = pt;
-                    return size;
-                }
-            }
-            if (up && down && pt < curr) {
-                curr = pt;
-                return size;
-            }
-            up = false;
-            down = false;
-            size = 1;
-            if (curr < pt) {
-                size++;
-                up = true;
-            }
-            curr = pt;
-            return 0;
-        }
-    }
-    
     public int longestMountain(int[] A) {
         int ans = 0;
-        int len = A.length;
-        Mountain mountain = new Mountain();
-        for(int i = 0; i < len; i++) {
-            ans = Math.max(ans, mountain.tryNextPoint(A[i]));
+        int currIdx = 0;
+        while (currIdx + 1 < A.length) {
+            int upSize = 1;
+            while (currIdx + 1 < len && A[currIdx] < A[currIdx + 1]) {
+                upSize++;
+                currIdx++;
+            }
+            if (upSize <= 1) {
+                currIdx++;
+                continue;
+            }
+            int downSize = 0;
+            while (currIdx + 1 < A.length && A[currIdx] > A[currIdx + 1]) {
+                downSize++;
+                currIdx++;
+            }
+            if (downSize > 0) {
+                ans = Math.max(ans, upSize + downSize);
+            }
         }
         return ans;
     }
