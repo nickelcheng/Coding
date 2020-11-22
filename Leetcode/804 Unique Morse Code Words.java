e60688fefcba231d7313fdb6547e72bd98da1f36
Nickel Cheng <nickelcheng@users.noreply.github.com>
2020-11-22 11:41:25 +0000

    Use hash set

--- Leetcode/804 Unique Morse Code Words.java
+++ Leetcode/804 Unique Morse Code Words.java
@@ -1,24 +1,16 @@
 class Solution {
     public int uniqueMorseRepresentations(String[] words) {
-        String[] morseCode = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String[] morseCode = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
         
-        String[] code = new String[words.length];
-        for (int i = 0; i < words.length; i++) {
-            code[i] = "";
-            for (int j = 0; j < words[i].length(); j++) {
-                code[i] += morseCode[words[i].charAt(j)-'a'];
+        Set<String> hash = new HashSet<>();
+        
+        for (String s : words) {
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < s.length(); i++) {
+                sb.append(morseCode[s.charAt(i)-'a']);
             }
+            hash.add(sb.toString());
         }
-        int ans = 0;
-        for (int i = 0; i < words.length; i++) {
-            boolean unique = true;
-            for (int j = i+1; j < words.length && unique; j++) {
-                if (code[i].equals(code[j])) {
-                    unique = false;
-                }
-            }
-            if (unique) ans++;
-        }
-        return ans;
+        return hash.size();
     }
 }
