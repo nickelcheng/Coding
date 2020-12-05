3e5f662bc0ea719c4a270f4fe93b7d1fe953c024
Nickel Cheng <nickelcheng@users.noreply.github.com>
2020-12-05 19:44:44 +0000

    Create 605 Can Place Flowers.java

---
+++ Leetcode/605 Can Place Flowers.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int len = flowerbed.length;
+        for (int i = 0; i < len && n > 0; i++) {
+            if (flowerbed[i] != 1) {
+                if ((i == 0 || flowerbed[i-1] == 0) && (i == len-1 || flowerbed[i+1] == 0)) {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+        }
+        return n <= 0;
+    }
+}
